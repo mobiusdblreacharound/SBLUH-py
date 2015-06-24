da940fc611b19f6c61cb2f1f4ab4dc7850950398
nutzchannel <spencie01@gmail.com>
2015-06-24 13:03:16 -0400

    Created a loop
    Resolves #1

--- sbluh.py
+++ sbluh.py
@@ -7,29 +7,28 @@ print "Python Edition"
 def unfin():
     print "I need to code this!"
     print "github.com/Nutzchannel/SBLUH-py/issues"
-    console()
 
 def console():
-    enterSpam = 0
-    stuff = raw_input("==> ")
-    if stuff == "help":
-        print "Available commands: help, aspect, bloodCalc, sweepCalc, playerData, and displayPlayerData"
-        print "Enter help:<command> for information on \"command\"."
-    elif stuff == "help:aspect":
-        print "[u] Displays your Classpect (Class and Aspect)."
-        console()
-    elif stuff == "aspect":
-        unfin()
-    elif stuff == "bloodCalc":
-        bloodCalc.main(False)
-    elif stuff == "sweepCalc":
-        unfin()
-    elif stuff == "playerData":
-        unfin()
-    elif stuff == "displayPlayerData":
-        unfin()
-    else:
-        print "'" + stuff + "' is not a valid command. Use \"help\" for information."
-        console()
+    while True:
+        enterSpam = 0
+        stuff = raw_input("==> ")
+        if stuff == "help":
+            print "Available commands: help, aspect, bloodCalc, sweepCalc, playerData, and displayPlayerData"
+            print "Enter help:<command> for information on \"command\"."
+        elif stuff == "help:aspect":
+            print "[u] Displays your Classpect (Class and Aspect)."
+            console()
+        elif stuff == "aspect":
+            unfin()
+        elif stuff == "bloodCalc":
+            bloodCalc.main(False)
+        elif stuff == "sweepCalc":
+            unfin()
+        elif stuff == "playerData":
+            unfin()
+        elif stuff == "displayPlayerData":
+            unfin()
+        else:
+            print "'" + stuff + "' is not a valid command. Use \"help\" for information."
 
 console()
