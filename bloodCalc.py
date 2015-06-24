796cbdb85f287135433ef7e453739d8dcc9aa302
nutzchannel <spencie01@gmail.com>
2015-06-23 21:28:51 -0400

    calculate() feature
    Almost done with bloodCalc!

--- bloodCalc.py
+++ bloodCalc.py
@@ -1,3 +1,42 @@
-__name__ = "bloodCalc"
 def test():
     print "bloodCalc OK!"
+
+def calculate(sign):
+    data = sign.upper()
+
+    x = 0
+
+    if data == "ARIES":
+        x = x + 1
+        return x
+    elif data == "TAURUS":
+        x = x + 2
+        return x
+    elif data == "GEMINI":
+        x = x + 3
+        return x
+    elif data == "CANCER":
+        x = x + 4
+        return x
+    elif data == "LEO":
+        x = x + 5
+        return x
+    elif data == "VIRGO":
+        x = x + 6
+        return x
+    elif data == "LIBRA":
+        x = x + 7
+        return x
+    elif data == "SCORPIO":
+        x = x + 8
+        return x
+    elif data == "SAGITTARIUS":
+        x = x + 9
+    elif data == "CAPRICORN":
+        x = x + 10
+    elif data == "AQUARIUS":
+        x = x + 11
+    elif data == "PISCES":
+        x = x + 12
+    else:
+        return 4
--- sbluh.py
+++ sbluh.py
@@ -1,4 +1,11 @@
 import json
 import bloodCalc
 
+print
+print
+print
+print "One second, let us set up..."
+print "- module tests! -"
+print
 bloodCalc.test()
+print bloodCalc.calculate("ARIEs")
