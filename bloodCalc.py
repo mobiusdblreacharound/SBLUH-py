267c27b349f6d3156baa42189425301dc5ec95c8
nutzchannel <spencie01@gmail.com>
2015-06-23 21:45:18 -0400

    Finished bloodCalc.find("","")
    BloodCalc should now be finished!

--- bloodCalc.py
+++ bloodCalc.py
@@ -40,3 +40,34 @@ def calculate(sign):
         x = x + 12
     else:
         return 4
+
+def find(sign1, sign2):
+
+    data = bloodCalc.calculate(sign1) + bloodCalc.calculate(sign2)
+
+    if data == 2 or data == 3:
+        return "Burgundy"
+    elif data == 4 or data == 5:
+        return "Brown"
+    elif data == 6 or data == 7:
+        return "Yellow"
+    elif data == 8:
+        return "Mutant/Candy Red"
+    elif data == 9:
+        return "Lime"
+    elif data == 10 or data == 11 or data == 13:
+        return "Green"
+    elif data == 12:
+        return "Jade"
+    elif data == 14 or data == 15:
+        return "Teal"
+    elif data == 16 or data == 17:
+        return "Cerulean"
+    elif data == 18 or data == 19:
+        return "Indigo"
+    elif data == 20 or data == 21:
+        return "Purple"
+    elif data == 22 or data == 23:
+        return "Violet"
+    elif data == 24:
+        return "Fuchsia"
