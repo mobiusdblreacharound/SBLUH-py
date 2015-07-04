b623f1ae5c4d7edd1b3098b5b4f33e3cce683b14
nutzchannel <spencie01@gmail.com>
2015-07-04 00:04:05 -0400

    Version updating

--- dpd.py
+++ dpd.py
@@ -2,7 +2,7 @@ import json
 def main():
     with open("save_data.json", "r") as sdf:
         saved = json.load(sdf)
-        print "You're running v0.1.1a2"
+        print "You're running v1.0.0"
         print "Your Chumhandle is " + saved["handle"] + "."
         print "Your blood color is " + saved["bloodColor"] + "."
         print "Your title is " + saved["classpect"]["class"] + " of " + saved["classpect"]["aspect"] + "."
